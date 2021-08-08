1e54c04dbe8f8a593a308b19d31167154a9366b1
ellywynn <whatisyouandme@mail.ru>
2021-08-09 01:01:42 +0300

    optimization

--- src/algorithms/bubble.ts
+++ src/algorithms/bubble.ts
@@ -1,4 +1,8 @@
 export default <T>(array: Array<T>): Array<T> => {
+  if (array.length < 2) {
+    return array;
+  }
+
   const temp = Array.from(array);
   for (let i = 0; i < temp.length; i++) {
     for (let j = 0; j < temp.length; j++) {
